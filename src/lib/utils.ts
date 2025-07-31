72c90a7d135810870dfaf4a3e3230a6ebf33715c
Nexly <work.nexly@gmail.com>
2025-07-31 10:02:30 +0000

    Indian rupree

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,5 +9,6 @@ export function formatCurrency(amount: number) {
   return new Intl.NumberFormat("en-IN", {
     style: "currency",
     currency: "INR",
+    minimumFractionDigits: 2,
   }).format(amount);
 }
