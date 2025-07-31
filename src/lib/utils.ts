5d1aab73396883a32bec4e94552658282c6d0206
Nexly <work.nexly@gmail.com>
2025-07-31 10:01:56 +0000

    The values will be in INR

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatCurrency(amount: number) {
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("en-IN", {
     style: "currency",
-    currency: "USD",
+    currency: "INR",
   }).format(amount);
 }
